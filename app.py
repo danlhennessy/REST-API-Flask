62b7e51d5b9836c24ac46605a965f1064bbae122
danlhennessy <danh118@hotmail.co.uk>
2022-07-22 23:07:22 +0100

    echo

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, url_for
+from flask import Flask, json, request, url_for
 app = Flask(__name__)
 
 @app.route('/')
@@ -19,6 +19,23 @@ def api_hello():
         return 'Hello ' + request.args['name']
     else:
         return 'Hello John Doe'
+
+@app.route('/echo', methods = ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'])
+def api_echo():
+    if request.method == 'GET':
+        return "ECHO: GET\n"
+
+    elif request.method == 'POST':
+        return "ECHO: POST\n"
+
+    elif request.method == 'PATCH':
+        return "ECHO: PATCH\n"
+
+    elif request.method == 'PUT':
+        return "ECHO: PUT\n"
+
+    elif request.method == 'DELETE':
+        return "ECHO: DELETE"
 
 if __name__ == '__main__':
     app.run()
