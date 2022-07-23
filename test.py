2dbc49425d5a19f1b82724291bbb13f09da0eaac
danlhennessy <danh118@hotmail.co.uk>
2022-07-23 10:10:22 +0100

    video api

--- test.py
+++ test.py
@@ -2,5 +2,5 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.get(BASE + "helloworld") # GET Request
+response = requests.get(BASE + "/helloworld/bill") # GET Request
 print(response.json())
