2143c3312a032b942c97207ba53ccd395083cd13
danlhennessy <danh118@hotmail.co.uk>
2022-07-23 15:12:50 +0100

    json= fix

---
+++ restful2.py
@@ -0,0 +1,24 @@
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
+
+app = Flask(__name__)
+api = Api(app)
+
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument("name", type=str, help="Name of video missing")
+video_put_args.add_argument("views", type=int, help="Views of video missing")
+video_put_args.add_argument("likes", type=int, help="Likes on video missing")
+
+videos = {}
+
+class Video(Resource):
+    def get(self, video_id):
+        return videos[video_id]
+    def put(self, video_id):
+        args = video_put_args.parse_args()
+        return {video_id: args}
+    
+api.add_resource(Video, "/video/<int:video_id>")
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import requests
 
 BASE = "http://127.0.0.1:5000/"
-#headers = {'Content-type': 'application/json'}
+#headers = {'Content-type': 'application/json; charset=utf-8'}
 
-response = requests.get(BASE) # PUT Request
+response = requests.put(BASE + 'video/1', json={"likes" : 10}) # PUT Request
 print(response.json())
